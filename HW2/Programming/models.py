ad16b21540fff2bbda81cf6aa3f3131ba0484e61
Chang Yan <cyan13@jhu.edu>
2021-10-13 07:55:59 -0400

    hw2 done

--- HW2/Programming/models.py
+++ HW2/Programming/models.py
@@ -5,7 +5,6 @@ This file is where you will write most of your code!
 """
 
 import numpy as np
-import math
 
 
 class Model(object):
@@ -69,60 +68,128 @@ class MCLogistic(MCModel):
 
     def __init__(self, *, nfeatures, nclasses):
         super().__init__(nfeatures=nfeatures, nclasses=nclasses)
-        # TODO: Implement this!
-        self.W = np.zeros((nfeatures + 1, nclasses - 1))
+        self.W = np.zeros((nfeatures + 1, len(nclasses) - 1))
 
 
     def fit(self, *, X, y, A, tol):
-        # TODO: Implement this!
         [numExamples, numFeatures] = np.shape(X)
-        X0 = np.vstack(np.ones((X.shape[1], 1)))
+        X = super()._fix_test_feats(X).toarray()
+        X0 = np.ones((X.shape[0], 1))
+        X0 = np.hstack((X0, X))
+        epoch = 0
         while(True):
-            predictions = self.predict(X)
+            predictions = self._predict(X0)
             jacobian = np.zeros((numFeatures + 1, 1))
             hessian = np.zeros((numFeatures + 1, numFeatures + 1))
 
             for i in range(numExamples):
                 for j in range(numFeatures + 1):
-                    jacobian[j, 0] += X0[i, j] * (y - predictions[i])
-
-            for i in range(numFeatures + 1):
-                for j in range(numFeatures + 1):
-                    for k in range(numFeatures + 1):
-                        hessian[i, j] += X0[k, i] * X0[k, j] * predictions[k] * (1 - predictions[k])
-
+                    jacobian[j, 0] += X0[i, j] * ( predictions[i] - y[i])  
+            jacobian /= numExamples
+            
+            for i in range(numExamples):
+                hessian += np.transpose(np.reshape(X0[i, :], (1, X0.shape[1]))) @ np.reshape(X0[i, :], (1, X0.shape[1])) * predictions[i] * (1 - predictions[i])   
+            hessian /= numExamples
             Wold = np.copy(self.W)
-            W = Wold - np.matmul(np.linalg.inv(hessian), jacobian)
-
-            if np.linalg.norm(W - Wold, axis = 1) < tol :
+            self.W = Wold - np.matmul(np.linalg.pinv(hessian), jacobian)
+            
+            loss = 0
+            for i in range(numExamples):
+                if y[i] == 1:
+                    loss += - np.log(predictions[i])
+                else:
+                    loss += - np.log(1-predictions[i])
+            loss /= numExamples
+            
+            print('epoch ' + str(epoch) + ': ' + str(loss))
+            
+            epoch += 1
+            if np.linalg.norm(self.W - Wold, axis = 0) < tol :
                 break
 
-
+    def _predict(self, X):
+        predictions = np.matmul(X, self.W)
+        for i in range(predictions.shape[0]):
+            predictions[i, 0] = 1 / (1 + np.exp(-predictions[i, 0]))
+        predictions = np.transpose(predictions)
+        predictions1 = np.zeros(predictions.shape[1])
+        for i in range(predictions.shape[1]):
+            predictions1[i] = predictions[0, i]
+        return predictions1;
+    
     def predict(self, X):
-        # TODO: Implement this!
-        X = super()._fix_test_feats(X)
-        X = np.vstack(np.ones((X.shape[1], 1)))
-        predictions = np.linalg.matmul(X, W)
-        for i in predictions:
-            i = 1 / (1 + math.exp(-i))
-        return np.transpose(predictions);
+        X = super()._fix_test_feats(X).toarray()
+        X0 = np.ones((X.shape[0], 1))
+        X0 = np.hstack((X0, X))
+        predictions = self._predict(X0)
+        predictions = predictions > 0.5
+        return predictions.astype(int);
+    
+    
+        
 
 class MCLogisticWithL2(MCModel):
 
     def __init__(self, *, nfeatures, nclasses):
         super().__init__(nfeatures=nfeatures, nclasses=nclasses)
-        # TODO: Implement this!
-        self.W = np.zeros((nfeatures, nclasses - 1))
+        self.W = np.zeros((nfeatures + 1, len(nclasses) - 1))
 
 
     def fit(self, *, X, y, A, tol):
-        # TODO: Implement this!
-        raise NotImplementedError()
+        [numExamples, numFeatures] = np.shape(X)
+        X = super()._fix_test_feats(X).toarray()
+        X0 = np.ones((X.shape[0], 1))
+        X0 = np.hstack((X0, X))
+        lamb = 10
+        epoch = 0
+        while(True):
+            predictions = self._predict(X0)
+            jacobian = np.zeros((numFeatures + 1, 1))
+            hessian = np.zeros((numFeatures + 1, numFeatures + 1))
 
+            for i in range(numExamples):
+                for j in range(numFeatures + 1):
+                    jacobian[j, 0] += X0[i, j] * ( predictions[i] - y[i])  
+            jacobian[1:-1, 0] += self.W[1:-1, 0]*lamb  
+            jacobian /= numExamples
+            
+            for i in range(numExamples):
+                hessian += np.transpose(np.reshape(X0[i, :], (1, X0.shape[1]))) @ np.reshape(X0[i, :], (1, X0.shape[1])) * predictions[i] * (1 - predictions[i]) 
+            I = np.identity(hessian.shape[0])
+            I[0,0] = 0
+            hessian += I*lamb
+            hessian /= numExamples
+            Wold = np.copy(self.W)
+            self.W = Wold - np.matmul(np.linalg.inv(hessian), jacobian)
+            
+            loss = 0
+            for i in range(numExamples):
+                if y[i] == 1:
+                    loss += - np.log(predictions[i])
+                else:
+                    loss += - np.log(1-predictions[i])
+            loss /= numExamples
+            
+            print('epoch ' + str(epoch) + ': ' + str(loss))
+            
+            epoch += 1
+            if np.linalg.norm(self.W - Wold, axis = 0) < tol :
+                break
 
+    def _predict(self, X):
+        predictions = np.matmul(X, self.W)
+        for i in range(predictions.shape[0]):
+            predictions[i, 0] = 1 / (1 + np.exp(-predictions[i, 0]))
+        predictions = np.transpose(predictions)
+        predictions1 = np.zeros(predictions.shape[1])
+        for i in range(predictions.shape[1]):
+            predictions1[i] = predictions[0, i]
+        return predictions1;
+    
     def predict(self, X):
-        X = self._fix_test_feats(X)
-        # TODO: Implement this!
-        predictions = np.zeros((X.shape[0], 1), dtype=np.int)
-
-        return predictions;
+        X = super()._fix_test_feats(X).toarray()
+        X0 = np.ones((X.shape[0], 1))
+        X0 = np.hstack((X0, X))
+        predictions = self._predict(X0)
+        predictions = predictions > 0.5
+        return predictions.astype(int);
