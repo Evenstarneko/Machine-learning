73b734d5fe373eb7c323a84242450873e72717cb
Chang Yan <cyan13@jhu.edu>
2021-10-28 21:20:51 -0400

    new model

--- HW3/sweep_channel.py
+++ HW3/sweep_channel.py
@@ -8,6 +8,6 @@ import os
 
 if __name__ == '__main__':
     for i in [10, 30, 50, 70, 90, 110, 130]:
-        command = "python main.py train --data-dir release-data --log-file cnn-logs-channels=" + str(i) + "csv --model-save cnn-channels=" + str(i) + ".torch --model simple-cnn --cnn-n1-channels " + str(i) 
+        command = "python main.py train --data-dir release-data --log-file cnn-logs-channels=" + str(i) + ".csv --model-save cnn-channels=" + str(i) + ".torch --model simple-cnn --cnn-n1-channels " + str(i) 
         os.system(command)
         
