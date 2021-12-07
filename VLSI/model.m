9320de72031bbfadf0505b0a7c6dae6946292fd9
Chang Yan <cyan13@jhu.edu>
2021-12-07 14:09:06 -0500

    Update model.m

--- VLSI/model.m
+++ VLSI/model.m
@@ -1,26 +1,36 @@
-dt = 0.0002;    % Time step
-t = 20;          % Max time
-I = 5e-3;        % Stimulus current
-a = 0.05;
+dt = 0.001;    % Time step
+t = 1.4;          % Max time
+a = -0.02;
 b = 0.1;
 c = -0.08;      % V_reset
-d = -0.75
-fei;     % Potassium channel potential
-tau = 1e-3;
-vt = -0.04;     % Threshold voltage
+d = -0.09;
+e = 10;
+tau = 5e-4;
+vt = -0.055;     % Threshold voltage
+I = 0:dt:t;
+I(100:length(I)) = 0.15;
+%I(100:length(I)) = linspace(0,1.5,1902);
+I(1:100) = 0;
+%I(1:length(I)) = 0;
+%I(100:180) = 0.15;
+%I(250:330) = 0.15;
+%I(1100:1180) = 0.15;
+%I(1550:1630) = 0.15;
 T = 0:dt:t;
 Vm = 0:dt:t;
-Vm(1) = -0.08;
-g = -0.75;
+Vm(1) = -0.07;
+g = 0;
 for t=1:length(T)-1
     if (Vm(t) >= vt)
+        Vm(t) = 0.04;
         Vm(t+1) = c;
-        g = g + d;
-    else
-        vp = I + a - b * Vm(t) + g * (d - Vm(t));
-        gp = -g / tau;
-        Vm(t+1) = Vm(t) + vp*dt;
+        gp = (-g + e) / tau;
         g = g + gp*dt;
+    else
+        gp = -g / tau;
+        g = g + gp*dt;
+        vp = I(t) + a - b * Vm(t) + g * (d - Vm(t));  
+        Vm(t+1) = Vm(t) + vp*dt;
     end
 end
 plot(T,Vm,'b-');
