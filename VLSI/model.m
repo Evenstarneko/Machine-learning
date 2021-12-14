c5d879ff6e2d98db68f717e8b646d8ee089fff74
Chang Yan <cyan13@jhu.edu>
2021-12-14 04:48:36 -0500

    vlsi model

--- VLSI/model.m
+++ VLSI/model.m
@@ -1,14 +1,14 @@
 dt = 0.001;    % Time step
-t = 1.4;          % Max time
-a = -0.02;
-b = 0.1;
-c = -0.08;      % V_reset
-d = -0.09;
-e = 10;
-tau = 5e-4;
-vt = -0.055;     % Threshold voltage
+t = 0.5;          % Max time
+a = -2;
+b = 10;
+c = -8;      % V_reset
+d = -9;
+e = 10000;
+tau = 2;
+vt = -5.5;     % Threshold voltage
 I = 0:dt:t;
-I(100:length(I)) = 0.15;
+I(100:length(I)) = 15;
 %I(100:length(I)) = linspace(0,1.5,1902);
 I(1:100) = 0;
 %I(1:length(I)) = 0;
@@ -18,21 +18,23 @@ I(1:100) = 0;
 %I(1550:1630) = 0.15;
 T = 0:dt:t;
 Vm = 0:dt:t;
-Vm(1) = -0.07;
-g = 0;
+Vm(1) = -7;
+g = 0:dt:t;
+g(1) = 0;
 for t=1:length(T)-1
     if (Vm(t) >= vt)
-        Vm(t) = 0.04;
+        Vm(t) = 4;
         Vm(t+1) = c;
-        gp = (-g + e) / tau;
-        g = g + gp*dt;
+        gp = (-g(t) + e) * tau;
+        g(t+1) = g(t) + gp*dt;
     else
-        gp = -g / tau;
-        g = g + gp*dt;
-        vp = I(t) + a - b * Vm(t) + g * (d - Vm(t));  
+        gp = -g(t) * tau;
+        g(t+1) = g(t) + gp*dt;
+        vp = I(t) + a - b * Vm(t) + g(t+1) * (d - Vm(t));  
         Vm(t+1) = Vm(t) + vp*dt;
     end
 end
-plot(T,Vm,'b-');
+figure()
+plot(T,Vm,'b-',T,g,'r-');
 xlabel('Time(s)');
 ylabel('Voltage (V)');
