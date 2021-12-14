a23e5fe8b07ef47a18ba61f84d7b60626ccf0d34
JL_DAZE <55678135+JL-DAZE@users.noreply.github.com>
2021-12-14 04:13:55 -0500

    w

--- Final Project/DataProcessing/ReadData.py
+++ Final Project/DataProcessing/ReadData.py
@@ -18,82 +18,89 @@ def main():
     savePath = './Data'
 
     numData = 0
+    k = 0
     for croppedImgName in os.listdir(croppedImgPath):
-        imgCropped = cv.imread(os.path.join(croppedImgPath, croppedImgName))
-        originalImgName = croppedImgName[0:-9]     # cutting '.chip.jpg' at the end
-        imgOriginal = cv.imread(os.path.join(originalImgPath, originalImgName))
-        if imgOriginal is not None:
-            [pts1, pts2] = ExtractKeypoints.extractKeypoints(imgOriginal, imgCropped, 25)
-            if pts1 is  None or pts2 is None and pts1.shape[0] >= 10 or pts2.shape[0] < 10:
-                continue
-            [H, status] = cv.findHomography(pts2, pts1, method = cv.RANSAC)
-            if H is None:
-                continue
-            # DisplayKeypoints.displayKeypoints(imgOriginal, imgCropped, pts1, pts2)
-
-            angle = abs(math.atan2(H[1,0], H[0,0])) * 180 / math.pi
-            if angle < 5:
-                originalShape = imgOriginal.shape
-                croppedShape = imgCropped.shape
-                if croppedShape[0] != 200 or croppedShape[1] != 200:
+        if k % 25 == 0:
+            imgCropped = cv.imread(os.path.join(croppedImgPath, croppedImgName))
+            originalImgName = croppedImgName[0:-9]     # cutting '.chip.jpg' at the end
+            imgOriginal = cv.imread(os.path.join(originalImgPath, originalImgName))
+            if imgOriginal is not None:
+                [pts1, pts2] = ExtractKeypoints.extractKeypoints(imgOriginal, imgCropped, 25)
+                if pts1 is  None or pts2 is None and pts1.shape[0] >= 10 or pts2.shape[0] < 10:
                     continue
-                croppedPts = np.array([[0, 0, 1], [0, croppedShape[1], 1], [croppedShape[0], croppedShape[1], 1], [croppedShape[0], 0, 1]])
-                originalPts = np.matmul(H, croppedPts.transpose()).transpose()
-                originalPts = np.round(originalPts[:, 0:2] / originalPts[:, 2:3]).astype(np.int32)
-                x1 = np.min(originalPts[:, 0]).clip(0, originalShape[1])
-                x2 = np.max(originalPts[:, 0]).clip(0, originalShape[1])
-                y1 = np.min(originalPts[:, 1]).clip(0, originalShape[0])
-                y2 = np.max(originalPts[:, 1]).clip(0, originalShape[0])
-                # DrawCropbox.drawCropbox(imgOriginal, originalPts, np.array([[x1, y1], [x2, y2]]))
-
-                labels = originalImgName.split('_', 3)
-                if not(labels[0].isdigit() and labels[1].isdigit() and labels[2].isdigit()):
+                [H, status] = cv.findHomography(pts2, pts1, method = cv.RANSAC)
+                if H is None:
                     continue
-                age = int(labels[0])
-                gender = int(labels[1])
-                race = int(labels[2])
-
-                ageClass = -1
-                if age <= 3:
-                    ageClass = 0
-                elif age <= 6:
-                    ageClass = 1
-                elif age <= 10:
-                    ageClass = 2
-                elif age <= 15:
-                    ageClass = 3
-                elif age <= 20:
-                    ageClass = 4
-                elif age <= 25:
-                    ageClass = 5
-                elif age <= 30:
-                    ageClass = 6
-                elif age <= 40:
-                    ageClass = 7
-                elif age <= 50:
-                    ageClass = 8
-                elif age <= 60:
-                    ageClass = 9
-                elif age <= 80:
-                    ageClass = 10
-                else:
-                    ageClass = 11
-
-                i = numData % 5;
-                j = int(numData / 5.0)
-
-                resizedShape = (500, (int)(originalShape[1] * 500.0 / originalShape[0]))
-                image = cv.resize(imgOriginal, resizedShape)
-                image = PreprocessImage.preprocess(image, False)
-                path = os.path.join(savePath, str(i), str(j))
-                np.savez_compressed(path, a = image)
-                imageCropped[i].append(PreprocessImage.preprocess(cv.resize(imgCropped, (224, 224)), False))
-                cropbox[i].append(np.array([x1, y1, x2, y2]) * 500.0 / originalShape[0])
-                label[i].append(np.array([ageClass, gender, race]))
-            
-                numData = numData + 1
-                if numData % 100 == 0:
-                    print(numData)
+                # DisplayKeypoints.displayKeypoints(imgOriginal, imgCropped, pts1, pts2)
+    
+                angle = abs(math.atan2(H[1,0], H[0,0])) * 180 / math.pi
+                if angle < 5:
+                    originalShape = imgOriginal.shape
+                    croppedShape = imgCropped.shape
+                    if croppedShape[0] != 200 or croppedShape[1] != 200:
+                        continue
+                    croppedPts = np.array([[0, 0, 1], [0, croppedShape[1], 1], [croppedShape[0], croppedShape[1], 1], [croppedShape[0], 0, 1]])
+                    originalPts = np.matmul(H, croppedPts.transpose()).transpose()
+                    originalPts = np.round(originalPts[:, 0:2] / originalPts[:, 2:3]).astype(np.int32)
+                    x1 = np.min(originalPts[:, 0]).clip(0, originalShape[1])
+                    x2 = np.max(originalPts[:, 0]).clip(0, originalShape[1])
+                    y1 = np.min(originalPts[:, 1]).clip(0, originalShape[0])
+                    y2 = np.max(originalPts[:, 1]).clip(0, originalShape[0])
+    
+                    labels = originalImgName.split('_', 3)
+                    if not(labels[0].isdigit() and labels[1].isdigit() and labels[2].isdigit()):
+                        continue
+                    age = int(labels[0])
+                    gender = int(labels[1])
+                    race = int(labels[2])
+    
+                    ageClass = -1
+                    if age <= 3:
+                        ageClass = 0
+                    elif age <= 6:
+                        ageClass = 1
+                    elif age <= 10:
+                        ageClass = 2
+                    elif age <= 15:
+                        ageClass = 3
+                    elif age <= 20:
+                        ageClass = 4
+                    elif age <= 25:
+                        ageClass = 5
+                    elif age <= 30:
+                        ageClass = 6
+                    elif age <= 40:
+                        ageClass = 7
+                    elif age <= 50:
+                        ageClass = 8
+                    elif age <= 60:
+                        ageClass = 9
+                    elif age <= 80:
+                        ageClass = 10
+                    else:
+                        ageClass = 11
+    
+                    i = numData % 5;
+                    j = int(numData / 5.0)
+    
+                    resizedShape = (250, (int)(originalShape[1] * 250.0 / originalShape[0]))
+                    imageResized = cv.resize(imgOriginal, (resizedShape[1], resizedShape[0]))
+                    imageProcessed = PreprocessImage.preprocess(imageResized, False)
+                    path = os.path.join(savePath, str(i), str(j))
+                    
+                    coeffX = 250.0 / originalShape[0]
+                    coeffY = resizedShape[1] / originalShape[1]
+                    cropboxResized = (np.array([x1 * coeffX, y1 * coeffY, x2 * coeffX, y2 * coeffY])).astype(int)
+                    imageCropped[i].append(PreprocessImage.preprocess(cv.resize(imgCropped, (224, 224)), False))
+                    
+                    np.savez_compressed(path, a = imageProcessed)
+                    cropbox[i].append(cropboxResized)
+                    label[i].append(np.array([ageClass, gender, race]))
+                    
+                    numData = numData + 1
+                    if numData % 100 == 0:
+                        print(numData)
+        k = k + 1
 
 
     for i in range(5):
